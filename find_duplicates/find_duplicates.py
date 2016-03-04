212d3c3d9eb2248bf4884c9c99ef9abead82d62e
Dan Hicks <hicks.daniel.j@gmail.com>
2016-03-04 11:32:05 -0500

    documentation

--- find_duplicates/find_duplicates.py
+++ find_duplicates/find_duplicates.py
@@ -1,3 +1,7 @@
+'''
+First run: identify potential duplicates, based on surnames
+Second run: collapse them?
+'''
 import json
 import pandas as pd
 import os.path as os
@@ -6,25 +10,34 @@ datafile = 'combined_metadata.json'
 potential_dupes_file = 'potential_dupes.csv'
 
 if not os.exists(potential_dupes_file):
+	# Load the data file
 	with open(datafile) as readfile:
 		authors = json.load(readfile)
 
+	# Convert to a Pandas data frame
 	authors_df = pd.DataFrame(authors)
+	# Remove some columns we won't need
 	del authors_df['areas']
 	del authors_df['docs']
+	# `name` is a column of dicts; break it out
 	authors_df['surname'] = pd.Series([author['name']['surname'] for author in authors])
 	authors_df['given'] = pd.Series([author['name']['given'] for author in authors])
+	# Convert surname to ascii, dropping non-ascii characters
 	authors_df['surname_ascii'] = authors_df['surname'].str.encode('ascii', 'ignore')
 	
+	# Identify ascii-ed surnames that appear more than once
 	surnames = set(authors_df['surname_ascii'].tolist())
 	surnames = [surname for surname in surnames 
 				if len(authors_df[authors_df['surname_ascii'] == surname]) > 1]
+	# Identify the authors with these ascii-ed surnames
 	authors_df = authors_df[authors_df['surname_ascii'].isin(surnames)]
 	authors_df = authors_df.sort_values('surname_ascii')
 	
+	# Clean up by removing the column of dicts and ascii-ed surnames
 	del authors_df['name']
 	del authors_df['surname_ascii']
 	
+	# Write to a CSV for manual checking
 	authors_df.to_csv(potential_dupes_file)
 
 else:
