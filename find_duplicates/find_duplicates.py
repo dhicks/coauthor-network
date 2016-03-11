6f20a15a9ef46c34bfac0564d4b35e5caa88a89c
Dan Hicks <hicks.daniel.j@gmail.com>
2016-03-11 08:25:27 -0500

    convert non-ascii characters to ascii, rather than dropping them

--- find_duplicates/find_duplicates.py
+++ find_duplicates/find_duplicates.py
@@ -5,6 +5,7 @@ Second run: collapse them?
 import json
 import pandas as pd
 import os.path as os
+import unicodedata
 
 datafile = 'combined_metadata.json'
 potential_dupes_file = 'potential_dupes.csv'
@@ -23,7 +24,10 @@ if not os.exists(potential_dupes_file):
 	authors_df['surname'] = pd.Series([author['name']['surname'] for author in authors])
 	authors_df['given'] = pd.Series([author['name']['given'] for author in authors])
 	# Convert surname to ascii, dropping non-ascii characters
-	authors_df['surname_ascii'] = authors_df['surname'].str.encode('ascii', 'ignore')
+	#authors_df['surname_ascii'] = authors_df['surname'].str.encode('ascii', 'ignore')
+	authors_df['surname_ascii'] = pd.Series(
+		[unicodedata.normalize('NFKD', surname).encode('ascii', 'ignore') 
+			for surname in authors_df['surname']])
 	
 	# Identify ascii-ed surnames that appear more than once
 	surnames = set(authors_df['surname_ascii'].tolist())
@@ -35,10 +39,42 @@ if not os.exists(potential_dupes_file):
 	
 	# Clean up by removing the column of dicts and ascii-ed surnames
 	del authors_df['name']
-	del authors_df['surname_ascii']
+	#del authors_df['surname_ascii']
 	
 	# Write to a CSV for manual checking
+	#print(authors_df)
 	authors_df.to_csv(potential_dupes_file)
 
 else:
  	print("You haven't written this branch yet!")
+ 	
+ 	'''
+ 	pseudocode:
+ 	
+ 	for node1, node2:
+ 		define new_node
+ 		for metadatum:
+ 			if metadatum == 'name':
+ 				retrieve preferred name from file
+ 			if metadatum == 'areas':
+ 				concatenate lists
+ 			elif metadatum == 'docs':
+ 				take sum
+ 			elif metadatum == 'country', 'affiliation', or 'sid':
+ 				replace the string with a list
+ 			elif metadatum[node1] == metadatum[node2]:
+ 				metadatum[new_node] = metadatum[node1]
+ 			elif metadatum[node1] == '':
+ 				metadatum[new_node] = metadatum[node2]
+ 			elif metadatum[node2] == '':
+ 				metadatum[new_node] = metadatum[node1]
+ 			else:
+ 				raise an exception
+ 		for node1 edge:
+ 			add edge between new_node and target
+ 		for node2 edge:
+ 			add edge between new_node and target
+ 		remove node 1
+ 		remove node 2
+ 	'''
+ 	
